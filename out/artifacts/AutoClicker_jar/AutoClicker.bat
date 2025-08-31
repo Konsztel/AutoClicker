7a522a4fdf3f690de333d778548d815fdc32459d
Konsztel <pawcio574@interia.pl>
2025-08-31 20:57:14 +0200

    Remove .idea and out from tracking

--- out/artifacts/AutoClicker_jar/AutoClicker.bat
+++
@@ -1,2 +0,0 @@
-java --module-path "C:\Programy\Java\Openj FX 24\lib" --add-modules javafx.controls -jar AutoClicker.jar
-pause
--- out/artifacts/AutoClicker_jar/Start.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run Chr(34) & "AutoClicker.bat" & Chr(34), 0
